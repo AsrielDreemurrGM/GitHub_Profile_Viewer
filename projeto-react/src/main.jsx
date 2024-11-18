ad6d75d686f958568bcbf20b0a24e95ec8e38583
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-11-18 10:54:48 -0300

    Visto Sobre Importação e Exportação
    - Visto Sobre Importação e Exportação de Arquivos e Variáveis;

--- projeto-react/src/main.jsx
+++ projeto-react/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Podemos mudar o nome de Variáveis importadas com o "as";
+import { PI as numeroPi, GTM_BRASIL } from './teste'
+import nome from './nomes'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
---
+++ projeto-react/src/nomes.js
@@ -0,0 +1,3 @@
+const nomes = ["Eduardo", "Jonas", "Roberto"]
+
+export default nomes;
