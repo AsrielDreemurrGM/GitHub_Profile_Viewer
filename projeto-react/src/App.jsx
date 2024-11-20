f2a7c7d5dd1281dd94b07eec8ee3f24fea366073
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-11-20 00:26:26 -0300

    Desativado Formulário
    - Desativado Formulário e Botão Pois Não Será Mais Utilizado;

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -3,11 +3,11 @@
 import { useState } from "react";
 
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario";
+// import Formulario from "./components/Formulario";
 import ReposList from "./components/ReposList";
 
 function App() {
-  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(false);
+  // const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(false);
   const [repositoriosEstaoVisiveis, setRepositoriosEstaoVisiveis] = useState(false);
   const [nomeUsuario, setNomeUsuario] = useState('');
 
@@ -25,10 +25,11 @@ function App() {
         </>
       )}
 
-      <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">Mostrar/Esconder Formulário</button>
+      {/* Mostrando Item Conform Sua Condição */}
+      {/* <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">Mostrar/Esconder Formulário</button>
       {formularioEstaVisivel && (
         <Formulario />
-      )}
+      )} */}
     </>
   )
 }
